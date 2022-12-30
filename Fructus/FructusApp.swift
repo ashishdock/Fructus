658c9e3a574385af31d8ea3e765e2d6a62a8b845
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2022-12-30 15:14:26 +0530

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Ashish Sharma on 12/30/2022.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
